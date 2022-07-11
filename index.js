d34c085fa3f542265b7497211422d32ad00b1634
ggk <guoguangkun@silexon.tech>
2022-07-11 14:08:33 +0800

    add package message

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+ * @moduleName: 
+ * @Author: Guoguang Kun
+ * @Date: 2022-07-11 14:06:49
+ * @LastEditTime: 2022-07-11 14:07:51
+ * @LastEditors: guoguangkun
+ * @Description: 
+ * 
+ * @FilePath: /jsScreenShot/index.js
+ * Copyright (c) 2022 by Guoguang Kun. 
+ */
+
+
+import {screenCapture} from './js/screenShot.js'
+
+
+export default screenCapture
